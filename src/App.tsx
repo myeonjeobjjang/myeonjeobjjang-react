d6235d35cd9fc70e11315cc33bf5293a3057e108
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-04-30 13:46:48 +0900

    tailwind 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import NotFound from "./pages/NotFound.tsx";
 import Bears from "./pages/Bears.tsx";
+import Header from "./components/Header.tsx";
 
 function App() {
   return (
     <>
+        <Header/>
         <Routes>
             <Route path={`/`} element={<Home/>}/>
             <Route path={`/bears`} element={<Bears/>}/>
--- src/pages/Bears.tsx
+++ src/pages/Bears.tsx
@@ -6,9 +6,11 @@ const Bears = () => {
     return (
         <div className={`Bears`}>
             <section>{bears}</section>
-            <button onClick={increasePopulation}>증가</button>
-            <button onClick={removeAllBears}>멸종</button>
-            <section>안녕!</section>
+            <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                    onClick={increasePopulation}>증가</button>
+            <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                    onClick={removeAllBears}>멸종</button>
+            <section className="text-5xl font-bold underline">안녕!</section>
         </div>
     )
 }
