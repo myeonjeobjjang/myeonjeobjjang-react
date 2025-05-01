548054d2531aed22944323182e8f6f126c10feab
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-05-02 00:30:23 +0900

    feat : 사용자 권한별 테마 변경 및 로고 글자 웹 폰트 설정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,68 @@
 import styled from "styled-components";
 import LogoIcon from "../assets/LogoIcon.tsx";
 import {useNavigate} from "react-router-dom";
-import {getAccessToken} from "../util/storage.ts";
+import {getAccessToken, getUserInfo} from "../util/storage.ts";
+import {useEffect, useState} from "react";
 
-const enum Colors {
-    LogoColor = "#ed7233",
-    HeaderUnderlineColor = "#974820",
-    LinkItemBackgroundColor = "#ea936c",
-    LinkItemTextColor = "#ffffff",
-    LinkItemTextHoverColor = "#783a1b",
+interface Colors {
+    LogoColor: string;
+    HeaderUnderlineColor: string;
+    LinkItemBackgroundColor: string;
+    LinkItemTextColor: string;
+    LinkItemTextHoverColor: string;
+}
+
+const NormalColors: Colors = {
+    LogoColor: "#ed7233",
+    HeaderUnderlineColor: "#974820",
+    LinkItemBackgroundColor: "#ea936c",
+    LinkItemTextColor: "#ffffff",
+    LinkItemTextHoverColor: "#783a1b"
+}
+
+const CompanyColors: Colors = {
+    LogoColor: "#335eed",
+    HeaderUnderlineColor: "#203897",
+    LinkItemBackgroundColor: "#6c76ea",
+    LinkItemTextColor: "#ffffff",
+    LinkItemTextHoverColor: "#1b2378"
+}
+
+const AdminColors: Colors = {
+    LogoColor: "#000000",
+    HeaderUnderlineColor: "#000000",
+    LinkItemBackgroundColor: "#000000",
+    LinkItemTextColor: "#ffffff",
+    LinkItemTextHoverColor: "#9f9f9f"
 }
 
 const Header = () => {
     const nav = useNavigate();
+    const [currentColor, setCurrentColor] = useState(NormalColors);
+
+    useEffect(() => {
+        const userInfo = getUserInfo();
+        if (!userInfo || (userInfo.role === "ROLE_APPLICANT")) {
+            setCurrentColor(NormalColors);
+        } else if (userInfo.role === "ROLE_COMPANY") {
+            setCurrentColor(CompanyColors);
+        } else if (userInfo.role === "ROLE_ADMIN") {
+            setCurrentColor(AdminColors);
+        }
+    });
+
     return (
-        <HeaderContainer underline_color={Colors.HeaderUnderlineColor} className="Header ">
+        <HeaderContainer underline_color={currentColor.LogoColor} className="Header ">
             <HeaderLogoContainer onClick={() => nav("/")}>
-                <LogoIcon color={Colors.LogoColor} height="35px" width="35px"/>
-                <HeaderLogoContent color={Colors.LogoColor}>면접짱</HeaderLogoContent>
+                <LogoIcon color={currentColor.LogoColor} height="35px" width="35px"/>
+                <HeaderLogoContent color={currentColor.LogoColor}>면접짱</HeaderLogoContent>
             </HeaderLogoContainer>
             <div>
                 <HeaderLinkList>
                     <HeaderLinkListItem>
                         <HeaderLinkListItemA
-                            color={Colors.LinkItemTextColor} background={Colors.LinkItemBackgroundColor}
-                            hover_color={Colors.LinkItemTextHoverColor}
+                            color={currentColor.LinkItemTextColor} background={currentColor.LinkItemBackgroundColor}
+                            hover_color={currentColor.LinkItemTextHoverColor}
                             onClick={() => nav("/")} aria-current="page">
                             Home
                         </HeaderLinkListItemA>
@@ -34,8 +72,9 @@ const Header = () => {
                             ( // 로그인 했을 경우
                                 <HeaderLinkListItem>
                                     <HeaderLinkListItemA
-                                        color={Colors.LinkItemTextColor} background={Colors.LinkItemBackgroundColor}
-                                        hover_color={Colors.LinkItemTextHoverColor}
+                                        color={currentColor.LinkItemTextColor}
+                                        background={currentColor.LinkItemBackgroundColor}
+                                        hover_color={currentColor.LinkItemTextHoverColor}
                                         onClick={() => nav("/profile")}>
                                         내 정보
                                     </HeaderLinkListItemA>
@@ -44,16 +83,18 @@ const Header = () => {
                                 <>
                                     <HeaderLinkListItem>
                                         <HeaderLinkListItemA
-                                            color={Colors.LinkItemTextColor} background={Colors.LinkItemBackgroundColor}
-                                            hover_color={Colors.LinkItemTextHoverColor}
+                                            color={currentColor.LinkItemTextColor}
+                                            background={currentColor.LinkItemBackgroundColor}
+                                            hover_color={currentColor.LinkItemTextHoverColor}
                                             onClick={() => nav("/signup")}>
                                             회원가입
                                         </HeaderLinkListItemA>
                                     </HeaderLinkListItem>
                                     <HeaderLinkListItem>
                                         <HeaderLinkListItemA
-                                            color={Colors.LinkItemTextColor} background={Colors.LinkItemBackgroundColor}
-                                            hover_color={Colors.LinkItemTextHoverColor}
+                                            color={currentColor.LinkItemTextColor}
+                                            background={currentColor.LinkItemBackgroundColor}
+                                            hover_color={currentColor.LinkItemTextHoverColor}
                                             onClick={() => nav("/login")}>
                                             로그인
                                         </HeaderLinkListItemA>
@@ -70,11 +111,9 @@ const Header = () => {
 export default Header;
 
 const HeaderContainer = styled.div<{ underline_color: string }>`
-    //background: darkgray;
     display: flex;
     flex-flow: row;
     padding: 0px;
-    //border-radius: 8px;
     margin: 5px;
     justify-content: space-between;
     border-bottom: 2px solid ${(props) => props.underline_color};
@@ -92,8 +131,15 @@ const HeaderLogoContainer = styled.a`
 `;
 
 const HeaderLogoContent = styled.span<{ color: string }>`
+    @font-face {
+        font-family: 'UhBeeSe_hyun';
+        src: url('https://gcore.jsdelivr.net/gh/projectnoonnu/noonfonts_five@.2.0/UhBeeSe_hyun.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    };
     color: ${(props) => props.color};
-    font-size: 25px;
+    font-size: 20px;
+    font-family: 'UhBeeSe_hyun', sans-serif;
 `;
 
 const HeaderLinkList = styled.ul`
