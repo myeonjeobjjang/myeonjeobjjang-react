f4005f1b90f7867fba69ce755d8c46af11100f89
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-05-01 19:00:31 +0900

    feat : 스타일드 컴포넌트 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,107 +1,99 @@
+import styled from "styled-components";
+import LogoIcon from "../assets/LogoIcon.tsx";
+
+const enum Colors {
+    LogoColor = "#ed7233",
+    HeaderUnderLineColor = "#974820",
+    LinkItemBackgroundColor = "#ea936c",
+    LinkItemTextColor = "#ffffff",
+}
+
 const Header = () => {
     return (
-        <div className="Header ">
-            <div
-                className="flex bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <a href="http://localhost:5173/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <img src="../../public/logo.svg" className="h-8" alt="면접짱 Logo"/>
-                        <span
-                            className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">면접짱</span>
-                    </a>
-                    <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-                         id="navbar-sticky">
-                        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                            <li>
-                                <a href="#"
-                                   className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                                   aria-current="page">
-                                    Home
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#"
-                                   className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                                    About
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#"
-                                   className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                                    Services
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#"
-                                   className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                                    Contact
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+        <HeaderContainer underLineColor={Colors.HeaderUnderLineColor} className="Header ">
+            <HeaderLogoContainer href="http://localhost:5173/">
+                <LogoIcon color={Colors.LogoColor} height="35px" width="35px"/>
+                <HeaderLogoContent color={Colors.LogoColor}>면접짱</HeaderLogoContent>
+            </HeaderLogoContainer>
+            <div>
+                <HeaderLinkList>
+                    <HeaderLinkListItem>
+                        <HeaderLinkListItemA
+                            color={Colors.LinkItemTextColor} background={Colors.LinkItemBackgroundColor}
+                            href="#" aria-current="page">
+                            Home
+                        </HeaderLinkListItemA>
+                    </HeaderLinkListItem>
+                    <HeaderLinkListItem>
+                        <HeaderLinkListItemA
+                            color={Colors.LinkItemTextColor} background={Colors.LinkItemBackgroundColor}
+                            href="#">
+                            About
+                        </HeaderLinkListItemA>
+                    </HeaderLinkListItem>
+                    <HeaderLinkListItem>
+                        <HeaderLinkListItemA
+                            color={Colors.LinkItemTextColor} background={Colors.LinkItemBackgroundColor}
+                            href="#">
+                            Services
+                        </HeaderLinkListItemA>
+                    </HeaderLinkListItem>
+                    <HeaderLinkListItem>
+                        <HeaderLinkListItemA
+                            color={Colors.LinkItemTextColor} background={Colors.LinkItemBackgroundColor}
+                            href="#">
+                            Contact
+                        </HeaderLinkListItemA>
+                    </HeaderLinkListItem>
+                </HeaderLinkList>
             </div>
-
-
-            {/*<nav*/}
-            {/*    className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">*/}
-            {/*    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">*/}
-            {/*        <a href="http://localhost:5173/" className="flex items-center space-x-3 rtl:space-x-reverse">*/}
-            {/*            <img src="../../public/logo.svg" className="h-8" alt="면접짱 Logo"/>*/}
-            {/*            <span*/}
-            {/*                className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">면접짱</span>*/}
-            {/*        </a>*/}
-            {/*        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">*/}
-            {/*            <button type="button"*/}
-            {/*                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get*/}
-            {/*                started*/}
-            {/*            </button>*/}
-            {/*            <button data-collapse-toggle="navbar-sticky" type="button"*/}
-            {/*                    className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"*/}
-            {/*                    aria-controls="navbar-sticky" aria-expanded="false">*/}
-            {/*                <span className="sr-only">Open main menu</span>*/}
-            {/*                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"*/}
-            {/*                     viewBox="0 0 17 14">*/}
-            {/*                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"*/}
-            {/*                          stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>*/}
-            {/*                </svg>*/}
-            {/*            </button>*/}
-            {/*        </div>*/}
-            {/*        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"*/}
-            {/*             id="navbar-sticky">*/}
-            {/*            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">*/}
-            {/*                <li>*/}
-            {/*                    <a href="#"*/}
-            {/*                       className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"*/}
-            {/*                       aria-current="page">*/}
-            {/*                        Home*/}
-            {/*                    </a>*/}
-            {/*                </li>*/}
-            {/*                <li>*/}
-            {/*                    <a href="#"*/}
-            {/*                       className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">*/}
-            {/*                        About*/}
-            {/*                    </a>*/}
-            {/*                </li>*/}
-            {/*                <li>*/}
-            {/*                    <a href="#"*/}
-            {/*                       className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">*/}
-            {/*                        Services*/}
-            {/*                    </a>*/}
-            {/*                </li>*/}
-            {/*                <li>*/}
-            {/*                    <a href="#"*/}
-            {/*                       className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">*/}
-            {/*                        Contact*/}
-            {/*                    </a>*/}
-            {/*                </li>*/}
-            {/*            </ul>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</nav>*/}
-
-        </div>
+        </HeaderContainer>
     )
 };
 
 export default Header;
+
+const HeaderContainer = styled.div<{ underLineColor: string }>`
+    //background: darkgray;
+    display: flex;
+    flex-flow: row;
+    padding: 0px;
+    //border-radius: 8px;
+    margin: 5px;
+    justify-content: space-between;
+    border-bottom: 2px solid ${(props) => props.underLineColor};
+`;
+
+const HeaderLogoContainer = styled.a`
+    text-decoration-line: none;
+    display: flex;
+    margin: 10px;
+    align-items: center;
+`;
+
+const HeaderLogoContent = styled.span<{ color: string }>`
+    color: ${(props) => props.color};
+    font-size: 25px;
+`;
+
+const HeaderLinkList = styled.ul`
+    list-style-type: none;
+    margin: 0px;
+    padding: 0px;
+    overflow: hidden;
+`;
+
+const HeaderLinkListItem = styled.li`
+    float: left;
+    padding: 10px;
+`;
+
+const HeaderLinkListItemA = styled.a<{ background: string, color: string }>`
+    background: ${(props) => props.background};
+    display: block;
+    color: ${(props) => props.color};
+    padding: 6px;
+    border-radius: 8px;
+    text-align: center;
+    text-decoration: none;
+`;
