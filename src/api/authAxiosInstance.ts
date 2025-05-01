4363fdcb0be92ed0bdb2f809ca41729177b65bdd
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-05-01 16:49:14 +0900

    feat : 토큰 사용시 axios 인스턴스 메서드 구현

---
+++ src/api/authAxiosInstance.ts
@@ -0,0 +1,59 @@
+import axios, {InternalAxiosRequestConfig} from "axios";
+import {getAccessToken, removeAccessToken, removeRefreshToken} from "../util/storage.ts";
+import {refreshTokens} from "./member/refreshTokens.ts";
+
+const authAxiosInstance = axios.create({
+    timeout: 10_000,
+    headers: {
+        "Content-Type": "application/json"
+    }
+});
+
+authAxiosInstance.interceptors.request.use(
+    (config: InternalAxiosRequestConfig) => {
+        const accessToken = getAccessToken();
+        if(accessToken && config.headers) {
+            config.headers["Authorization"] = `Bearer ${accessToken}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+)
+
+authAxiosInstance.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    async (error) => {
+        const {response: {status, data : {errorCode, message, timeStamp}} } = error;
+        console.log(`Error Response ${status} : ${errorCode} ${message} [${timeStamp}]`, error);
+        const originRequest = error.config;
+        if(status === 401) {
+            try {
+                const newToken = await refreshTokens().catch((error) => {
+                    throw error;
+                });
+
+                if(newToken) {
+                    originRequest.headers.Authorization = `Bearer ${newToken.jwtPairResponse.accessToken}`;
+                    return authAxiosInstance(originRequest);
+                }
+            } catch(error) {
+                console.log("토큰 갱신 실패 ", error);
+                removeAccessToken();
+                removeRefreshToken();
+                window.location.href = "/login";
+                return Promise.reject(error);
+            }
+        } else {
+            if(!getAccessToken()) { // access token 이 없는 경우
+                window.location.href = "/login";
+            }
+        }
+        return Promise.reject(error);
+    }
+)
+
+export default authAxiosInstance;
