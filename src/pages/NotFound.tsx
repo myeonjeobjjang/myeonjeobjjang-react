3934e71713d276c58403690f9cce84deb6b91116
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-05-02 16:14:53 +0900

    feat : not-found 페이지 이동 시 path 변경 설정

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,4 +1,15 @@
+import usePageTitle from "../hooks/usePageTitle.ts";
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
+
 const NotFound = () => {
+    const nav = useNavigate();
+    useEffect(() => {
+        if(location.pathname !== "/not-found") {
+            nav("/not-found", {replace: true});
+        }
+    }, [location.pathname])
+    usePageTitle(`페이지를 찾을 수 없음`);
     return (
         <div className={`Not_Found`}>
             Not Found
